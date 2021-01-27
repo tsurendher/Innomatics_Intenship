7186b94328bf744de227c9b2a27908c360a41cc8
tsurendher <77716408+tsurendher@users.noreply.github.com>
2021-01-27 23:55:39 +0530

    Add files via upload

---
+++ day_5/1_DetectFloatingPointNumber.py
@@ -0,0 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+for _ in range(int(input())):
+    print(bool(re.match(r'^[-+]?[0-9]*\.[0-9]+$', input())))
---
+++ day_5/4_re.Findall_re.finditer.py
@@ -0,0 +1,5 @@
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+s = '[qwrtypsdfghjklzxcvbnm]'
+a = re.findall('(?<=' + s +')([aeiou]{2,})' + s, input(), re.I)
